b60b092a3eb352807629d1986e0d147932a88a15
Kim Burgess <kim@acaprojects.com>
2019-08-12 23:24:22 +1000

    add spec for #ping

--- spec/docker/api/client.cr
+++ spec/docker/api/client.cr
@@ -1,8 +1,9 @@
 require "../../spec_helper"
 
 describe Docker::Api::Client do
+  client = Docker::Api::Client.new
+
   describe "base HTTP methods" do
-    client = Docker::Api::Client.new
     {% for method in %w(get post put head delete patch options) %}
       it "{{method.id}}" do
         WebMock.stub :{{method.id}}, "#{client.client.host}/foo"
@@ -13,19 +14,25 @@ describe Docker::Api::Client do
   end
 
   it "supports specifying a custom API version" do
-    client = Docker::Api::Client.new version: "v1.23"
-    WebMock.stub :any, "#{client.client.host}/#{client.version}/foo"
-    response = client.get "/foo"
+    versioned_client = Docker::Api::Client.new version: "v1.23"
+    WebMock.stub :any, "#{versioned_client.client.host}/#{versioned_client.version}/foo"
+    response = versioned_client.get "/foo"
     response.status_code.should eq(200)
   end
 
   it "raises an exception on error response" do
-    client = Docker::Api::Client.new
     WebMock.stub(:get, "#{client.client.host}/foo").to_return do
       HTTP::Client::Response.new(500, body: "{ \"message\": \"This is an error\" }")
     end
     expect_raises(Docker::ApiError, "This is an error") do
       client.get "/foo"
+    end
+  end
+
+  describe "#ping" do
+    it "supports base ping query" do
+      WebMock.stub(:get, "#{client.client.host}/_ping").to_return body: "OK"
+      client.ping.should be_true
     end
   end
 end
