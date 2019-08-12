e5fce138f751089131dfe37edbd2e44d7bf6cc04
Kim Burgess <kim@acaprojects.com>
2019-08-12 22:13:49 +1000

    make initialization params visible on base Docker module methods

--- src/docker.cr
+++ src/docker.cr
@@ -5,16 +5,26 @@ module Docker
 
   # Creates a docker client instance based on setting from env vars.
   def from_env
-    Docker::Client.new ENV["DOCKER_HOST"]
+    client ENV["DOCKER_HOST"]
   end
 
-  # Creates a new docker client instance
-  def client(*kwargs)
-    Docker::Client.new *kwargs
+  # Creats a new docker client for the local docker socket.
+  def client
+    Docker::Client.new
   end
 
-  # Creates a new low-level docker API wrapper
-  def api_client(*kwargs)
-    Docker::Api::Client.new *kwargs
+  # Creates a new docker client instance for *base_url*.
+  def client(base_url : String)
+    Docker::Client.new base_url
+  end
+
+  # Creates a new low-level docker API wrapper the local docker socket.
+  def api_client
+    Docker::Api::Client.new
+  end
+
+  # Creates a new low-level docker API wrapper for *base_url*
+  def api_client(base_url)
+    Docker::Api::Client.new base_url
   end
 end
--- src/docker/client.cr
+++ src/docker/client.cr
@@ -1,8 +1,14 @@
 require "./api/client"
 
+# User friendly client for type-safe interaction with the Docker Engine API.
 class Docker::Client
   private getter client : Docker::Api::Client
 
+  # Creats a new instance for a pre-establish low-level client.
+  def initialize(@client)
+  end
+
+  # Creates a new instance for the passed *base_url*
   def initialize(base_url = Docker::Api::Client::DEFAULT_URL)
     @client = Docker::Api::Client.new base_url
   end
