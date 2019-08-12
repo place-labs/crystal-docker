c57cc3d724bba77adceac7efe83bbed2b61fd68b
Kim Burgess <kim@acaprojects.com>
2019-08-13 00:10:17 +1000

    remove setters from version model

--- src/docker/api/models/version.cr
+++ src/docker/api/models/version.cr
@@ -2,15 +2,15 @@ require "json"
 
 struct Docker::Api::Models::Version
   JSON.mapping({
-    version:         {key: "Version", type: String},
-    os:              {key: "Os", type: String},
-    kernel_version:  {key: "KernelVersion", type: String},
-    go_version:      {key: "GoVersion", type: String},
-    git_commit:      {key: "GitCommit", type: String},
-    arch:            {key: "Arch", type: String},
-    api_version:     {key: "ApiVersion", type: String},
-    min_api_version: {key: "MinAPIVersion", type: String},
-    build_time:      {key: "BuildTime", type: String},
-    experimental:    {key: "Experimental", type: Bool},
+    version:         {setter: false, key: "Version", type: String},
+    os:              {setter: false, key: "Os", type: String},
+    kernel_version:  {setter: false, key: "KernelVersion", type: String},
+    go_version:      {setter: false, key: "GoVersion", type: String},
+    git_commit:      {setter: false, key: "GitCommit", type: String},
+    arch:            {setter: false, key: "Arch", type: String},
+    api_version:     {setter: false, key: "ApiVersion", type: String},
+    min_api_version: {setter: false, key: "MinAPIVersion", type: String},
+    build_time:      {setter: false, key: "BuildTime", type: String},
+    experimental:    {setter: false, key: "Experimental", type: Bool},
   })
 end
