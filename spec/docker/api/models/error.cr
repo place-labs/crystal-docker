d05470711b669aadd19b64e36cd9924ee1794587
Kim Burgess <kim@acaprojects.com>
2019-08-15 16:03:48 +1000

    run formatter tool

--- spec/docker/api/models/error.cr
+++ spec/docker/api/models/error.cr
@@ -2,11 +2,11 @@ require "../../../spec_helper"
 
 describe Docker::Api::Models::Error do
   it "parses from JSON" do
-    Docker::Api::Models::Error.from_json( <<-JSON
-      {
-        "message": "page not found"
-      }
-    JSON
+    Docker::Api::Models::Error.from_json(<<-JSON
+     {
+       "message": "page not found"
+     }
+   JSON
     ).should be_a(Docker::Api::Models::Error)
   end
 end
