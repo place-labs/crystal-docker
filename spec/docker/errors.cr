1058ad581795bba0efcc90db64cc019a05fc540b
Kim Burgess <kim@acaprojects.com>
2019-08-19 09:52:25 +1000

    add spec for api errors

---
+++ spec/docker/errors.cr
@@ -0,0 +1,10 @@
+require "../spec_helper"
+
+describe Docker::ApiError do
+  it "build exceptions from HTTP responses" do
+    response = HTTP::Client::Response.new 500, { message: "foo" }.to_json
+    exception = Docker::ApiError.from_response response
+    exception.message.should eq("foo")
+    exception.status_code.should eq(500)
+  end
+end
