7c9624aa5ce419ad00736bb24f061c5a335bbcf8
Kim Burgess <kim@acaprojects.com>
2019-08-19 11:27:01 +1000

    fix formatting

--- spec/docker/errors.cr
+++ spec/docker/errors.cr
@@ -2,7 +2,7 @@ require "../spec_helper"
 
 describe Docker::ApiError do
   it "build exceptions from HTTP responses" do
-    response = HTTP::Client::Response.new 500, { message: "foo" }.to_json
+    response = HTTP::Client::Response.new 500, {message: "foo"}.to_json
     exception = Docker::ApiError.from_response response
     exception.message.should eq("foo")
     exception.status_code.should eq(500)
