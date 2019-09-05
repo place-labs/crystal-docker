8540602a665c66b6ea2d74842a92852ac1a9e5ab
Kim Burgess <kim@acaprojects.com>
2019-09-05 18:15:06 +1000

    have build return an iterator of build output

--- src/docker/api/images.cr
+++ src/docker/api/images.cr
@@ -10,16 +10,27 @@ module Docker::Api::Images
   # process, allowing for building on remote hosts or environments the docker daemon does not have
   # direct read access to the target.
   #
-  # Yields an iterator of `BuildInfo` objects that will contain updates from the daemon as the
-  # image build progresses.
+  # Returns an iterator of `Models::BuildInfo` objects with the build output.
   def build(path : String, **opts)
     params = HTTP::Params.encode opts
 
     headers = HTTP::Headers.new
     headers["Content-Type"] = "application/x-tar"
 
-    post "/build?#{params}", headers: headers, body: Tools.tar path do |response|
-      yield response.body_io.each_line.map(&->Models::BuildInfo.from_json(String))
+    channel = Channel(Iterator(Models::BuildInfo)).new
+
+    spawn do
+      post "/build?#{params}", headers: headers, body: Tools.tar path do |response|
+        # Responses are returned as JSON-lines in a streamed response body
+        line_iter  = response.body_io.each_line
+        build_info = line_iter.map &->Models::BuildInfo.from_json(String)
+        channel.send build_info
+      end
     end
+
+    # FIXME: this is creation infinite recursion in the compiler - need to put a workaround in
+    # place that doesn't pass the iterator to the channel.
+    # see: https://github.com/crystal-lang/crystal/issues/8150
+    channel.receive
   end
 end
--- src/docker/images.cr
+++ src/docker/images.cr
@@ -8,12 +8,13 @@ class Docker::Images
   #
   # Similar to the `docker build` command.
   def build(path = String, **props)
-    client.build path, **props do |build_info|
-      # NOTE: despite the API docs specifying an object with seperate fields for error, progress
-      # etc, all info appears to come back with the stream atribute. To keep things clean, only
-      # this is extracted at this point.
-      build_info.each { |update| yield update.stream }
-    end
+    updates = client.build path, **props
+
+    # NOTE: despite the API docs specifying an object with seperate fields for error, progress etc,
+    # all info appears to come back with the stream atribute. To keep things clean, extract this
+    # attribute as a stand-alone string.
+    updates.each { |update| yield update.stream }
+
     # TODO: await response and parse to an image object
   end
 end
