39fa2ca574a2953becb2492bfc276d58d18dc320
Caspian Baska <caspianbaska@gmail.com>
2019-09-13 16:24:08 +1000

    Docker::Containers#list

--- src/docker/containers.cr
+++ src/docker/containers.cr
@@ -16,6 +16,22 @@ class Docker::Containers
     Container.new client, response.id
   end
 
+  # List containers. Similar to the docker ps command.
+  #
+  # Available `filters`, filter value may be a single value or an array:
+  # - exited : Int32     Only containers with specified exit code
+  # - status : String    One of "restarting", "running", "paused", "exited"
+  # - label : String     Format either "key" or "key=value"
+  # - id : String        Container ID
+  # - name : String      Container name
+  # - ancestor : String  String Filter by container ancestor. <image-name>[:tag], <image-id>, <image@digest>.
+  # - before : String    String Only containers created before a particular container. Container name or ID.
+  # - since : String     Only containers created after a particular container. Container name or ID.
+  def list(all : Bool? = nil, limit : Int? = nil, size : Bool? = nil, filters : Hash? = nil) : Array(Container)
+    container_ids = client.containers(all, limit, size, filters).map &.id
+    container_ids.map &->get(String)
+  end
+
   # Get a container by name or ID.
   def get(id_or_name)
     container = client.inspect_container id_or_name
